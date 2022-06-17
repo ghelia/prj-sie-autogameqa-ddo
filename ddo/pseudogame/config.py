4b6dc964ce73dc86beee46a6abd27199b0372bbc
Merlin Dugot <merlin.dugot@ghelia.com>
2022-06-17 10:00:11 +0900

    datapath argument

--- ddo/pseudogame/config.py
+++ ddo/pseudogame/config.py
@@ -1,9 +1,5 @@
 class PGConfig:
 
-    csv = [
-        "./data/traj1/data.csv",
-        "./data/traj3/data.csv"
-    ]
     frame_obs_idx = [-3, -1, 0]
     size = 224
     hidden_layer = [128]
--- train_pg.py
+++ train_pg.py
@@ -1,19 +1,28 @@
 import os
+import argparse
 
 from ddo.ddo import ddo
 from ddo.config import Config
 from ddo.recorder import Recorder
 from ddo.pseudogame.network import PGAgent
 from ddo.pseudogame.data import ExpertData
-from ddo.pseudogame.config import PGConfig
 
 
 
-if not os.path.exists(os.path.join("./saves", Config.session)):
-    os.makedirs(os.path.join("./saves", Config.session))
-recorder = Recorder(os.path.join("./logs", Config.session))
-save_path = os.path.join("./saves", Config.session)
-# agent = DebugAgent()
-data = ExpertData(PGConfig.csv)
-agent = PGAgent()
-ddo(agent, recorder, save_path, data)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train a DDO model for Pseudogame")
+    parser.add_argument("datapath", type=str, help="path of the directory containing CSV files of the expert")
+    args = parser.parse_args()
+    csvs = []
+    for root, dirs, files in os.walk(args.datapath):
+        for file in files:
+            if file.endswith(".csv"):
+                csvs.append(os.path.join(root, file))
+    print("Expert data : ", csvs)
+    if not os.path.exists(os.path.join("./saves", Config.session)):
+        os.makedirs(os.path.join("./saves", Config.session))
+    recorder = Recorder(os.path.join("./logs", Config.session))
+    save_path = os.path.join("./saves", Config.session)
+    data = ExpertData(csvs)
+    agent = PGAgent()
+    ddo(agent, recorder, save_path, data)
